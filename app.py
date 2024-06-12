76e7283df67888bf1b269bc9b66b588d7eb6902d
Lionel du Peloux <lionpeloux@users.noreply.github.com>
2024-06-12 17:13:18 +0200

    download file from obs

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
 import streamlit as st
+import requests
 
-dpe_id = st.text_input("Numéro de DPE")
+dpe_id = '2369E3640698P'
+dpe_id = st.text_input("Numéro de DPE", value=dpe_id)
+
+b = st.button("Obtenir")
+if b:
+
+    url = f"https://observatoire-dpe-audit.ademe.fr/pub/dpe/{dpe_id}/xml"
+    headers = {
+        'Accept': 'text/xml',
+    }
+
+    response = requests.get(url, headers=headers)
+    st.write(f"url : `{url}`")
+    st.write(response.content)
+
+
+if __name__ == "__main__":
+
+    dpe_id = '2369E3640698P'
+    url = f"https://observatoire-dpe-audit.ademe.fr/pub/dpe/{dpe_id}/xml"
+    headers = {
+        'Accept': 'text/xml',
+    }
+
+    def download_1(url):
+        from urllib.request import urlopen, urlretrieve
+        import cgi
+
+        remotefile = urlopen(url)
+        contentdisposition = remotefile.info()['Content-Disposition']
+        _, params = cgi.parse_header(contentdisposition)
+        filename = params["filename"]
+        urlretrieve(url, filename)
+
+    download_1(url)
+
+    response = requests.get(url, allow_redirects=True)
+    print(response.headers.get('Content-Disposition'))
+    # print(f"url : `{url}`")
+    # print(response.content)
+
+    # https://www.itersdesktop.com/2020/09/04/downloading-files-in-python-using-requests-module/
