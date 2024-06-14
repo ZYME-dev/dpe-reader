db58b81b67177d9e0d53e3680f67bf40379e1031
Lionel <lionpeloux@users.noreply.github.com>
2024-06-15 00:17:09 +0200

    version streamlit basic fonctionnelle. Principe de dispatch num version

--- app.py
+++ app.py
@@ -1,84 +1,140 @@
 import streamlit as st
 import requests
 
-dpe_id = '2369E3640698P'
-dpe_id = '2344E0308327N'
+
+def download_xmlstring(dpe_id:str) -> str:
+    """Télécharge le xml d'un DPE à partir de son ID depuis la plateforme 
+    observatoire-dpe-audit.ademe.fr
+    """
+
+    # https://stackoverflow.com/questions/2795331/python-download-without-supplying-a-filename
+    from urllib.request import urlopen
+    import pyrfc6266
+
+    url = f"https://observatoire-dpe-audit.ademe.fr/pub/dpe/{dpe_id}/xml"
+    headers = {
+        'Accept': 'text/xml',
+    }
+    
+    data = urlopen(url)
+    info = data.info()
+    filename = pyrfc6266.parse_filename(info['Content-Disposition'])
+    xmlstring = data.read()
+    return xmlstring
+
+
+def get_xmltree(xmlstring:str):
+    """Obtient les identifiants du DPE et le numéro de version et retourne 
+    le xml sous forme de xmltree (lxml).
+
+    Le numéro de version est nécessaire pour savoir quelle schéma/modèle 
+    utiliser.
+
+    Un cleaning est pratiqué pour enlever :
+        - les nodes 'status' et 'numero_dpe' qui ne sont pas conforme au model
+        - les nodes 'data_complementaires' qui provoquent une erreur lors du parse
+    """
+
+    from lxml import etree
+
+    xmltree = etree.ElementTree(etree.fromstring(xmlstring))
+    root = xmltree.getroot()
+    numero_dpe = xmltree.xpath('numero_dpe')[0]
+    statut = xmltree.xpath('statut')[0]
+    enum_version_id = xmltree.xpath('administratif/enum_version_id')[0]
+    
+    return (numero_dpe.text, statut.text, enum_version_id.text, xmltree)
+
+
+def load_model(xmltree, enum_version_id):
+    """Sérialize le xml dans un modèle python (dataclass). 
+    Principe du dispatch selon la méthode utilisée.
+    """
+    
+    from xsdata.formats.dataclass.parsers import XmlParser 
+    import models.dpe_v2_2
+    import models.dpe_v2_3
+    
+    if enum_version_id == "2.2":
+        Dpe = models.dpe_v2_2.Dpe
+    elif enum_version_id == "2.3":
+        Dpe = models.dpe_v2_3.Dpe
+    else:
+        raise Exception(f"La version {enum_version_id} du DPE n'est pas prise en charge.")
+    
+
+    # do some cleaning ...
+    # sinon erreur lors de la déserialization xml > dataclass
+
+    root = xmltree.getroot()
+
+    numero_dpe = xmltree.xpath('numero_dpe')[0]
+    root.remove(numero_dpe)
+
+    statut = xmltree.xpath('statut')[0]
+    root.remove(statut)
+
+    for c in xmltree.xpath('//data_complementaires'): 
+        c.getparent().remove(c)
+    
+    # parse xmltree to model
+    parser = XmlParser()
+    file_path = f"assets/{dpe_id}.xml"
+    model = parser.parse(xmltree, Dpe)
+
+    return model
+
+
+st.title("DPE")
+
+st.caption("pour obtenir les informations d'un DPE, saisissez son numéro d'identification")
+
+dpe_id = '2369E3640698P' # v2.3
+dpe_id = '2344E0308327N' # v2.2
 dpe_id = st.text_input("Numéro de DPE", value=dpe_id)
 
 b = st.button("Obtenir")
+
 if b:
 
-    url = f"https://observatoire-dpe-audit.ademe.fr/pub/dpe/{dpe_id}/xml"
-    headers = {
-        'Accept': 'text/xml',
-    }
+    xmlstring = download_xmlstring(dpe_id)
+    numero_dpe, statut, enum_version_id, xmltree = get_xmltree(xmlstring)
+    model = load_model(xmltree, enum_version_id)
 
-    response = requests.get(url, headers=headers)
-    st.write(f"url : `{url}`")
-    st.write(response.content)
+   
+    st.text_input("Version", value=enum_version_id)
+    st.text_input("Statut", value=statut)
 
+    st.subheader("Murs")
+    murs = model.logement.enveloppe.mur_collection.mur
+    st.write(murs)
+    # for mur in murs:
+    #     print(mur.donnee_entree.surface_paroi_totale)
+
+    st.subheader("Plancher Bas")
+    pb = model.logement.enveloppe.plancher_bas_collection.plancher_bas
+    st.write(pb)
+    
 
 if __name__ == "__main__":
 
-    dpe_id = '2369E3640698P'
-    url = f"https://observatoire-dpe-audit.ademe.fr/pub/dpe/{dpe_id}/xml"
-    headers = {
-        'Accept': 'text/xml',
-    }
+    # dpe_id = '2344E0308327N' # v2.2
+    dpe_id = '2369E3640698P' # v2.3  
 
-    def download(url):
-        
-        # https://stackoverflow.com/questions/2795331/python-download-without-supplying-a-filename
-        from urllib.request import urlopen, urlretrieve
-        from lxml import etree
-        import pyrfc6266
-        
-        data = urlopen(url)
-        info = data.info()
-        filename = pyrfc6266.parse_filename(info['Content-Disposition'])
-        xmlstring = data.read()
-        et = etree.ElementTree(etree.fromstring(xmlstring))
-        # et.write('./tmp/output.xml', pretty_print=True)
-        root = et.getroot()
-        for child in root:
-            if child.tag in ["numero_dpe", "statut"] :
-                root.remove(child)
-            #     continue
-            # if True:# TODO: do your check here!
-            #     root.remove(child)
-        for child in root:
-            print(child)
-        urlretrieve(url, './tmp/output.xml')
-        return (xmlstring, et)
-        pass
-
-    from bs4 import BeautifulSoup
-       
-    xmlstring = download(url)
-    soup = BeautifulSoup(xmlstring, 'xml')
-    r1 = soup.find("numero_dpe")
-    r2 = soup.find("statut")
-    print(r1)
-    print(r2)
-
-    with open('tmp/pretty.xml', "w") as f:
-        f.write(soup.prettify())
-        f.close()
-
+    xmlstring = download_xmlstring(dpe_id)
+    numero_dpe, statut, enum_version_id, xmltree = get_xmltree(xmlstring)
     
-    from models_dataclass import Dpe
-    from xsdata.formats.dataclass.parsers import XmlParser
+    model = load_model(xmltree, enum_version_id)
 
-    parser = XmlParser()
-    result = parser.parse("tmp/2344E0308327N.xml", Dpe)
+    for child in xmltree.getroot():
+            print(child)
 
-    murs = result.logement.enveloppe.mur_collection.mur
+    print(f"N°      : {numero_dpe}")
+    print(f"Version : {enum_version_id}")
+    print(f"Statut  : {statut}")
+
+    murs = model.logement.enveloppe.mur_collection.mur
     for mur in murs:
         print(mur.donnee_entree.surface_paroi_totale)
 
 
-
-    # print(f"url : `{url}`")
-    # print(response.content)
-
-    # https://www.itersdesktop.com/2020/09/04/downloading-files-in-python-using-requests-module/
